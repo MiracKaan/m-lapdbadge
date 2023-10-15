56b92ffcd2c7fe60b629432e06c15041179f43e0
MirageWasHere <63926390+MiracKaan@users.noreply.github.com>
2023-10-15 21:15:56 +0300

    Add files via upload

---
+++ m-lapdbadge/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+author 'Mirage'
+description 'Police Badge Mirage'
+
+game 'gta5'
+
+client_scripts {
+    'cl_badge.lua'
+}
+
+server_scripts {
+    'sv_badge.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/assets/css/*.css',
+	'html/assets/js/*.js',
+	'html/assets/fonts/roboto/*.woff',
+	'html/assets/fonts/roboto/*.woff2',
+	'html/assets/fonts/justsignature/JustSignature.woff',
+	'html/assets/images/*.png'
+}
